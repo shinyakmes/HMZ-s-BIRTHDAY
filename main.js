be91f60e9495741ae1ea135bcf09df32260e3357
shinyakmes <141297427+shinyakmes@users.noreply.github.com>
2025-12-19 01:44:51 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,415 @@
+// 数字组合对应的信件内容映射
+const letterContents = {
+    '1226': {
+        title: '生日快乐喵！',
+        content: `
+            <p>致我人生中遇见过最美丽的星星：</p>
+            <br>
+            <p>时间过得真的很快很快，又多了一岁</p>
+            <p>祝黑米粥在新的一岁里多创造自己的辉煌喵！虎鲸期待你的蜕变喵！</p>
+            <p>我希望宝宝一直快乐，一直幸福地活下去</p>
+            <br>
+            <p>借以此言，我会一直陪在你身边，好好爱你</p>
+ 	   <p>平安喜乐，万事顺意！</p>
+        `,
+        signature: '小虎鲸'
+    },
+    '2005': {
+        title: '我们可是同龄！',
+        content: `
+            <p>致最特别的你：</p>
+            <br>
+            <p>宝宝真聪明喵！想到这个了！</p>
+            <p>虎鲸比你大了八个月，虎鲸一定一定会做好年上的职责的！</p>21:16 2025/12/16
+            <p>请一直当我的公主殿下好不好qaq</p>
+            <p>虎鲸会一直宠你</p>
+            <br>
+            <p>一直一直</p>
+        `,
+        signature: '大虎鲸哥哥'
+    },
+    '5201': {
+        title: '我喜欢你',
+        content: `
+            <p>致我深爱的宝宝·：</p>
+            <br>
+            <p>说起来有些丢人......</p>
+            <p>不知道从什么时候开始，虎鲸自己悄悄陷进去了</p>
+            <br>
+            <p>或许喜欢就是这种感觉吧？你不在身边的每一秒每分钟都好煎熬好煎熬，止不住去想你再想你</p>
+            <p>感谢宝宝选择了我，接纳了我的不完美，虎鲸真的很开心很开心</p>
+            <br>
+	    <p>虎鲸平日里的时候总喜欢把永远放在嘴边<p>
+            <p>我当然没办法设法相信这个世界上有永远</p>
+ 	    <p>但是在和你相处的每一天，听着宝宝的声音和享受宝宝的爱</p>
+            <p>我还是觉得我们一定要永远永远在一起</p>
+            <p>因为我想  <br>   用我最初的心，陪你走最远的路</p>
+             <br>  
+	    <p>我爱你，直到永远</p>
+        `,
+        signature: '李深源'
+    },
+    '0000': {
+        title: '开发幕后喵',
+        content: `
+            <p>虎鲸做这个的主意是在十二月十四的那天晚上，虎鲸是一个很注重仪式感的人，对于朋友也好恋人也罢，送出礼物的时候总觉得必须要出一份足够匹得上的心意，虎鲸很喜欢这只黑米粥，所以就想出了这样的一份礼物，这可是独一份！不知道黑米粥是否喜欢，其实也有点害怕啊哈哈？这可是我整过的超级无敌大活！AI真是太好用了你们知道吗！哈哈哈我编了大半天不如问ai几句话，只能说佛物虎鲸是这样的，学了好久的前端开发还是没能精通，就写了这一些简单的小玩意，虎鲸写字特丑，用这个还算是省事的！正在听Ahead of Us 的虎鲸超有干劲是这样</p>	
+	     <p>不过很想告诉黑米粥的一件事是，如果黑米粥觉得很亏欠而不知道怎么回礼的时候，其实准备礼物的虎鲸在制造惊喜的时候，已经在脑海里幻想着宝宝无数次惊喜和幸福的模样了，所以不会存在亏欠喵！也不需要什么同等的回报啦！所以请宝宝不要有任何负担！所有的所有只是因为宝宝在我的心目中很珍贵很珍贵，宝宝放开心去享受吧！虎鲸爱你喵！</p> `,
+    },
+'1025': { title: '坏结局', content: '<p>既然有5201，那为什么没有1025呢？</p><p>这是我感情之下潜藏的阴影</p><p>这是我钦定的唯一坏结局</p><br><p>你好耀眼</p><p>耀眼到虎鲸觉得自己只是茫茫人海中的过客，就像之前聊到hadew那样：我知道他和我在一起的话根本不可能</p><p>回想起刚认识那一天晚上，虎鲸做了一个好长好长的甜蜜的梦，如同命运捉弄般的相遇，每一次心灰意冷的时候又有新的惊喜出现，虎鲸本来说不上非常非常喜欢，但是被一次次的巧合打动了</p><p>在为虎鲸歌唱的那天晚上，虎鲸觉得这一辈子永远忘不了你的好</p><p>不要......不要离开我 昨天也好明天也罢，虎鲸同样同样真的不想放手，一想到我们的未来总是黑暗</p><p>宝宝身边的人都如此优秀，为什么选择的是我....完全搞不懂，虎鲸找不到被爱的理由，虎鲸从小到大都是一个人孤零零的</p><p>你为什么喜欢我？明明我相比之下完全没有任何优点....随时可以被替换被抛弃，虎鲸害怕，当每次悄悄假装不经意间提及这个的时候，宝宝总是打着哈哈擦过去，虎鲸鼓起勇气说出的那些话在心里拧成了一团</p><p>很抱歉在这个生日礼物里塞了这种烂货，这明明就是烂活嘛！但是虎鲸想了好久还是打算加进来，就算是虎鲸在制作里的焦虑吧(?)希望我们能敞开心扉去说一些大家都不适应的话，虎鲸会做出改变，那样我们才能够一定能够走远......真害怕生日那天挖到这段，算了，让它埋在过去的沙子里，再也出不来，不过虎鲸不会放手的，除非黑米粥主动提出放弃，虎鲸会穷尽所有能做的手段去挽回的！</p><p>你要让时间.....怎么转......</p>', signature: '我不会放手的' },
+    '1234': {
+        content: `
+            <p>你输入这个干啥！</p>
+            <p>这是彩蛋喵！</p>
+            <br>
+            <p>如果您能看到这封信，说明：</p>
+            <p>√虎鲸和ai共同完成的输入系统工作正常</p>
+            <p>✓ 虎鲸做的内容显示功能正常</p>
+            <p>✓ 黑米粥已经点进来了</p>
+            <br>
+            <p>那命令黑米粥  1234爱上我喵！</p>
+            <br>
+            <p>我ok了，上号！</p>
+        `,
+        signature: 'AI真是太好用了你们知道吗！~'
+    },
+    '8888': {
+        title: '发发发发发',
+        content: `
+            <p>给这只幸运的黑米粥：</p>
+            <br>
+            <p>虎鲸本来对这个数字没什么感觉，但是不得不说当时给出这份前兆生日礼物(马内)的时候，就已经觉得这数字不赖了喵！</p>
+            <p>看到这个数字的黑米粥立刻拿出刮刮乐刮中一播完！(虎鲸心虚中)</p>
+            <p>那虎鲸只能祝愿宝宝：</p>
+            <p>• 学业蒸蒸日上</p>
+            <p>• 和我甜甜蜜蜜！</p>
+            <p>• 健康平安喜乐</p>
+            <p>• 天天赚钱笑哈哈</p>
+	    <p>• 天天和虎鲸赚超多哈弗币！</p>
+            <br>
+            <p>爱笑的女孩子运气不会太差喵！</p>
+            <br>
+            <p>多笑一下啦！碰到不开心的事情要及时闪开哦！这也算是幸运的一环！</p>
+        `,
+        signature: '米米哈基米'
+    },
+'6666': {
+ title: '66666666666666',
+  content: 
+    '<p>666666666666666666666666666666666666666666666666666666666<br>666666666666666666666666666666666666666666</p>' +
+    '<br>' +
+    '<div class="text-center mt-4">' +
+      '<a href="https://www.bilibili.com/video/BV1GJ411x7h7/" ' +
+         'target="_blank" ' +
+         'class="inline-block px-4 py-2 bg-pink-500 text-white rounded hover:bg-pink-600">' +
+         '点我领取大几几' +
+      '</a>' +
+    '</div>',
+},
+  '1314': {
+  content: `
+    <p>总搜这些干什么！虎鲸是来庆生不是来表白的喵！</p>
+    <p>但是虎鲸确实超级喜欢你，不然可不会花这么多时间去搞喵！</p>
+    <p>虎鲸每一次听到特关消息都是超级无敌大惊喜，也许我才是焦虑型恋人啊？！每次宝宝一说话，虎鲸就急着听到下一句话，不回消息自己快焦虑死了哈哈哈哈，不过也和宝宝道歉，做这个之前虎鲸把聊天记录从头翻到尾了一遍，确实是虎鲸在初期太冷淡了，对不起宝宝.....</p>
+       <p>黑米粥说过当有一位能接受黑米粥控制欲的人出现就会在一起，那请允许虎鲸走进宝宝的生活里</p>
+<p>如果说宝宝是假装深思熟虑其实是恋爱脑，那虎鲸就是表面恋爱脑背后深思熟虑。其实虎鲸打算全心爱你，是虎鲸自己做的最正确的决定，我超爱这个黑米粥，巴不得每天都粘着宝宝，喜欢你带来的独特感觉，欣赏你刀子嘴豆腐心的性格，害怕你受伤，对伤害你的人万分愤怒，每一次对宝宝说我爱你不只是我轻飘飘的调情，虎鲸从来不会将这种话说得很轻....所以，请允许虎鲸更加更加爱黑米粥好不好！</p>
+        <br>
+    <p>倘若命运垂青，我可以爱你到白头</p>
+    <p>骗你的，不允许我也会爱，爱你到我生命的最后一刻</p>
+    <br>
+    <p>生日快乐，我可爱的黑米粥宝宝</p>
+  `,
+},
+  '1009': {
+        content: `
+            <p>这是我们认识的日子</p>
+            <p>如果时间允许，我可以一直回到这里吗？</p>
+            <p>那是一段虎鲸被黑米粥追逐的日子，虎鲸恐惧却又欣喜若狂的日子，虎鲸是第一次网恋，当时被（核武器大手）吓坏了哈哈哈哈哈</p>
+            <p>当时虎鲸是出于回应黑米粥感情的念想去做的</p>
+            <p>黑米粥当时总是改口，又说做朋友又说男闺蜜，距离忽远忽近虎鲸被吊的又高又低，虎鲸已经没有办法再作出决定，但是虎鲸感觉好甜蜜好甜蜜，虎鲸第一次被这样喜欢被这样爱着</p>
+            <p>虎鲸也有自己的情感创伤，所以虎鲸很怕只是暧昧，即使它没有成本，我们可以享受我们的全部，可是你不属于我</p>
+            <p>越是暧昧甜蜜，你不属于我，我就感觉到窒息，凭什么相互喜欢却不能顺着感觉继续。虎鲸享受着黑米粥的喜欢直到现在，谢谢你黑米粥，感动得不知道该说什么好</p>
+            <p>现在我们两个已经是一对了吧！我就说我们能淌过来的！其实是虎鲸才想明白自己在给自己使绊子）虎鲸的债可不轻易那么容易结清的，虎鲸会.....一直会陪着你，我们往前走</p>
+	 <p>回望我们的甜蜜，而属于我们的幸福在现在才刚刚开始</p>
+		<p>虎鲸爱你</p>
+        `,
+        signature: '虎鲸'
+    },
+    '1125': {
+        title: '如果你在生日当天，请立即退出！',
+        content: `
+            <p>还记得这个时刻吗？</p>
+            <br>
+            <p>这是我们第一次吵架吧？</p>
+            <p>那次试探踩踏到了虎鲸的创伤，虎鲸真的哈气了</p>
+            <p>虎鲸嫉妒黑米粥的任何一位前任还有白月光，恨他们没有好好对黑米粥，同时又庆幸他们给了我一次被爱的机会</p>
+            <p>虎鲸知道黑米粥对前任的ptsd始于初中时候，那个时候我不敢回答，只是沉默而过，我从来没有谈过一场正式的恋爱</p>
+            <p>和黑米粥的恋爱是第一次</p>
+            <p>虎鲸拿性命去担保</p>
+            <p>虎鲸的那些破事如果黑米粥想听，虎鲸讲给黑米粥听</p>
+	    <p>所以啊......</p>
+            <p>当时谁在街边哭鼻子啦？！虎鲸可不会不要你，黑米粥是虎鲸的小公主，虎鲸拼尽全力去守护的小公主！</p>
+            <p>虎鲸真的很爱你，想陪宝宝一直一直过下去</p>
+	 <p>所以以后吵架的时候，答应虎鲸不要做伤害自己的事情，无论谁对谁错</p>
+		 <p>然后，等虎鲸来赎罪，我会摆平一切</p>
+        `,
+        signature: '是我'
+    },
+  '0401': {
+        content: `
+            <p>诶嘿？</p>
+            <br>
+            <p>这是我生日哦？客串啦！</p>
+            <p>生日快乐喵！虎鲸可比你大了270天左右哦！</p>
+            <p>回忆起别人总说我的出生是个笑话啊哈哈哈哈哈</p>
+            <p>而虎鲸觉得生在这个日子里是为了创造快乐，让别人能发自内心的笑也是一种本事好不好！所以虎鲸很喜欢传播阳光和欢声笑语，让大家笑起来哦</p>
+            <p>黑米粥也笑一个！快点嘛！</p>
+        `,
+        signature: 'idiot'
+    },
+ '1130': {
+        content: `
+            <p>宝宝唱歌给我听啦！</p>
+            <p>虎鲸第一次被女孩子这样唱歌，还是在虎鲸最脆弱的时候</p>
+            <p>如果这是gal的话，虎鲸那会已经好感度直接bang！的一声爆了！</p>
+            <p>现在想起来还依旧感动得说不出话</p>
+            <p>虎鲸当时可是听着宝宝羞涩的声音眼泪哗啦啦往下流，这世界上怎么会有这么可爱的女孩子喵！给虎鲸一个大男人感动得痛哭流涕啊哈哈哈</p>
+            <p>即使说过好多好多次，虎鲸还是想说，虎鲸真的很爱很爱你，这份独属于我的浪漫，我不会忘掉</p>
+        `,
+        signature: '至死不渝'
+    },
+ '1201': {
+        content: `
+            <p>今天是宝宝的第一个非洲之心出生的日子！</p>
+            <p>虎鲸和宝宝一起玩三角洲68天了喵！</p>
+            <p>还记得虎鲸和你撤出来的机密第一把</p>
+            <p>虎鲸在救你死死都不松手，这辈子都不想放开</p>
+            <p>我们这样一看也算出生入死的兄弟了喵！</p>
+	     <p>我们一起欢笑过，陪着宝宝生气过，出大红紧张过</p>
+	     <p>宝宝总是说害怕，当时还记得虎鲸那句话怎么说吗？</p>
+	      <p>：起来，舔包，然后我们回家好不好？</p>
+   	     <p>我们回家</p>
+		<br>
+		 <p>确认包裹上路，欢迎回家</p>
+        `,
+        signature: '你的引航人'
+    },
+ '9999': {
+        content: `
+            <p>开发幕后2(?)</p>
+            <p>这可真是个大工程！</p>
+            <p>好在这一切总算告一段落了</p>
+            <p>怎么样？虎鲸的杰作怎么样！</p>
+            <p>生日快乐喵！黑米粥sama</p>
+	     <p>虎鲸还想写很多很多，还有好多好多事情想和黑米粥做</p>
+	     <p>虎鲸会一直陪你一岁又一岁的</p>
+	      <p>不止今天喵！</p>
+   	     <p>好啦，话到这里就结束了喵！</p>
+		<br>
+		 <p>生日快乐！</p>
+        `,
+        signature: 'sign to love''
+    },
+
+};
+
+// DOM元素
+const inputs = [
+    document.getElementById('digit1'),
+    document.getElementById('digit2'),
+    document.getElementById('digit3'),
+    document.getElementById('digit4')
+];
+const submitBtn = document.getElementById('submitBtn');
+const btnText = document.getElementById('btnText');
+const loadingSpinner = document.getElementById('loadingSpinner');
+const message = document.getElementById('message');
+const hintText = document.getElementById('hintText');
+
+// 初始化动画
+document.addEventListener('DOMContentLoaded', function() {
+    // 页面加载动画
+    anime({
+        targets: '.fade-in',
+        opacity: [0, 1],
+        translateY: [30, 0],
+        duration: 800,
+        delay: anime.stagger(200),
+        easing: 'easeOutQuart'
+    });
+    
+    // 自动聚焦第一个输入框
+    setTimeout(() => {
+        inputs[0].focus();
+    }, 1000);
+});
+
+// 输入框事件处理
+inputs.forEach((input, index) => {
+    // 只允许输入数字
+    input.addEventListener('input', function(e) {
+        const value = e.target.value;
+        if (!/^\d$/.test(value)) {
+            e.target.value = '';
+            return;
+        }
+        
+        // 输入动画
+        anime({
+            targets: e.target,
+            scale: [1, 1.1, 1],
+            duration: 300,
+            easing: 'easeOutElastic(1, .6)'
+        });
+        
+        // 自动跳到下一个输入框
+        if (value && index < inputs.length - 1) {
+            inputs[index + 1].focus();
+        }
+        
+        checkComplete();
+    });
+    
+    // 键盘事件处理
+    input.addEventListener('keydown', function(e) {
+        if (e.key === 'Backspace' && !e.target.value && index > 0) {
+            inputs[index - 1].focus();
+            inputs[index - 1].value = '';
+        } else if (e.key === 'Enter') {
+            checkAndSubmit();
+        }
+    });
+    
+    // 聚焦事件
+    input.addEventListener('focus', function() {
+        anime({
+            targets: e.target,
+            scale: 1.05,
+            duration: 200,
+            easing: 'easeOutQuart'
+        });
+    });
+    
+    // 失焦事件
+    input.addEventListener('blur', function() {
+        anime({
+            targets: e.target,
+            scale: 1,
+            duration: 200,
+            easing: 'easeOutQuart'
+        });
+    });
+});
+
+// 检查输入是否完成
+function checkComplete() {
+    const allFilled = inputs.every(input => input.value !== '');
+    
+    if (allFilled) {
+        submitBtn.disabled = false;
+        btnText.textContent = '决定了！就是这个！';
+        
+        // 显示提示
+        anime({
+            targets: hintText,
+            opacity: [0, 1],
+            duration: 300
+        });
+        
+        // 按钮动画
+        anime({
+            targets: submitBtn,
+            scale: [1, 1.02, 1],
+            duration: 500,
+            easing: 'easeOutElastic(1, .6)'
+        });
+    } else {
+        submitBtn.disabled = true;
+        btnText.textContent = 'typing~';
+        
+        // 隐藏提示
+        anime({
+            targets: hintText,
+            opacity: [1, 0],
+            duration: 300
+        });
+    }
+}
+
+// 检查并提交
+function checkAndSubmit() {
+    const code = inputs.map(input => input.value).join('');
+    
+    if (code.length !== 4) {
+        showError('凑够四个啦！');
+        return;
+    }
+    
+    if (letterContents[code]) {
+        showSuccess();
+        setTimeout(() => {
+            goToLetter(code);
+        }, 1500);
+    } else {
+        showError('诶嘿，没有提供的义务！');
+        shakeInputs();
+    }
+}
+
+// 显示错误信息
+function showError(text) {
+    btnText.textContent = text;
+    submitBtn.classList.add('bg-red-600', 'hover:bg-red-500');
+    submitBtn.classList.remove('bg-stone-800', 'hover:bg-stone-700');
+    
+    setTimeout(() => {
+        btnText.textContent = '让我看看哪个比较顺眼！';
+        submitBtn.classList.remove('bg-red-600', 'hover:bg-red-500');
+        submitBtn.classList.add('bg-stone-800', 'hover:bg-stone-700');
+    }, 2000);
+}
+
+// 显示成功状态
+function showSuccess() {
+    btnText.classList.add('hidden');
+    loadingSpinner.classList.remove('hidden');
+    submitBtn.classList.add('bg-green-600');
+    submitBtn.classList.remove('bg-stone-800');
+}
+
+// 输入框震动效果
+function shakeInputs() {
+    inputs.forEach(input => {
+        input.classList.add('error-shake');
+        setTimeout(() => {
+            input.classList.remove('error-shake');
+        }, 500);
+    });
+    
+    // 清空输入
+    setTimeout(() => {
+        inputs.forEach(input => input.value = '');
+        inputs[0].focus();
+        checkComplete();
+    }, 600);
+}
+
+// 跳转到信件页面
+function goToLetter(code) {
+    // 保存信件内容到localStorage
+    localStorage.setItem('currentLetter', JSON.stringify(letterContents[code]));
+    localStorage.setItem('letterCode', code);
+    
+    // 页面跳转动画
+    anime({
+        targets: 'body',
+        opacity: [1, 0],
+        duration: 500,
+        easing: 'easeInQuart',
+        complete: function() {
+            window.location.href = 'letter.html';
+        }
+    });
+}
+
+// 提交按钮点击事件
+submitBtn.addEventListener('click', checkAndSubmit);
